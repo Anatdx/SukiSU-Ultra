0dd04a13efa2d99e0d3e3098796e96492ff97d73
Anatdx <an@anatdx.com>
2025-12-25 17:37:53 +0800

    kernel: add SuperKey checks to manager.h
    - Add superkey_is_manager() check in is_manager()
    - Add superkey_get_manager_uid() check in ksu_get_manager_uid()
    - Add superkey_invalidate() in ksu_invalidate_manager_uid()
    - Update ksu_is_manager_uid_valid() for SuperKey mode
    
    This was missing and caused SuperKey authentication to not work
    even when SuperKey IOCTL was available.

--- kernel/manager.h
+++ kernel/manager.h
@@ -13,20 +13,41 @@ extern void ksu_add_manager(uid_t uid, int signature_index);
 extern void ksu_remove_manager(uid_t uid);
 extern int ksu_get_manager_signature_index(uid_t uid);
 
+// SuperKey 支持
+#ifdef CONFIG_KSU_SUPERKEY
+#include "superkey.h"
+#endif
+
 static inline bool ksu_is_manager_uid_valid(void)
 {
+#ifdef CONFIG_KSU_SUPERKEY
+	// 超级密码模式：检查是否有已认证的管理器
+	return superkey_get_manager_uid() != (uid_t)-1 ||
+	       ksu_manager_uid != KSU_INVALID_UID;
+#else
 	return ksu_manager_uid != KSU_INVALID_UID;
+#endif
 }
 
 #ifndef CONFIG_KSU_SUSFS
 static inline bool is_manager(void)
 {
+#ifdef CONFIG_KSU_SUPERKEY
+	// 超级密码模式优先
+	if (superkey_is_manager())
+		return true;
+#endif
 	return unlikely(ksu_is_any_manager(current_uid().val) || 
 			(ksu_manager_uid != KSU_INVALID_UID && ksu_manager_uid == current_uid().val));
 }
 #else
 static inline bool is_manager()
 {
+#ifdef CONFIG_KSU_SUPERKEY
+	// 超级密码模式优先
+	if (superkey_is_manager())
+		return true;
+#endif
 	return unlikely((ksu_manager_uid == current_uid().val % 100000) || 
 			(ksu_manager_uid != KSU_INVALID_UID && ksu_manager_uid == current_uid().val % 100000));
 }
@@ -34,6 +55,11 @@ static inline bool is_manager()
 
 static inline uid_t ksu_get_manager_uid(void)
 {
+#ifdef CONFIG_KSU_SUPERKEY
+	uid_t superkey_uid = superkey_get_manager_uid();
+	if (superkey_uid != (uid_t)-1)
+		return superkey_uid;
+#endif
 	return ksu_manager_uid;
 }
 
@@ -52,6 +78,9 @@ static inline void ksu_set_manager_uid(uid_t uid)
 static inline void ksu_invalidate_manager_uid(void)
 {
 	ksu_manager_uid = KSU_INVALID_UID;
+#ifdef CONFIG_KSU_SUPERKEY
+	superkey_invalidate();
+#endif
 }
 
 int ksu_observer_init(void);
